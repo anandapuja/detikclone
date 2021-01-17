b01f9549838fbfa37c318c124cda9d4bcfe7936d
anandapuja <anandapujawandra@gmail.com>
2021-01-17 12:26:51 +0700

    done clone detikcom using next js and deploy with vercell

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,64 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import MainHomeBig from "../components/HomeComponents/MainHomeBig";
+import MainHomeGrid from "../components/HomeComponents/MainHomeGrid";
+import MainHomeList from "../components/HomeComponents/MainHomeList";
+import TopHomeGrid from "../components/HomeComponents/TopHomeGrid";
+import BacaJugaPosts from "../components/SidebarComponents/BacaJugaPosts";
+import PopulerPosts from "../components/SidebarComponents/PopulerPosts";
 
-export default function Home() {
+export default function Home({
+  posts,
+  topPosts,
+  mainPost,
+  relatedPosts,
+  mainGridPosts,
+}) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>detikcom - Informasi Berita Terkini dan Terbaru Hari Ini</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <Header />
+      <TopHomeGrid topPosts={topPosts} />
+      <div className="w-full sm:w-4/5 m-auto sm:flex">
+        <div className="w-full sm:w-3/5">
+          <MainHomeBig mainPost={mainPost} relatedPosts={relatedPosts} />
+          <div className="py-4">
+            <MainHomeList posts={topPosts} />
+            <MainHomeGrid posts={mainGridPosts} />
+            <MainHomeList posts={posts} />
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className="w-full sm:w-2/5">
+          <PopulerPosts posts={topPosts} />
+          <BacaJugaPosts posts={mainGridPosts} />
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const sbPost = await fetch(
+    "https://aeroindomagz.com/wp-json/wp/v2/posts?_embed"
+  );
+  const posts = await sbPost.json();
+  const topPosts = posts.slice(0, 5);
+  const mainGridPosts = posts.slice(0, 6);
+  const mainPost = posts[0];
+  const relatedPosts = posts.slice(1, 3);
+  return {
+    props: {
+      posts,
+      topPosts,
+      mainPost,
+      relatedPosts,
+      mainGridPosts,
+    },
+    revalidate: 1,
+  };
 }
